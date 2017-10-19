144832e2c32fd1649775541f8b9b658515cb4a0b
Ivan <roni10b@abv.bg>
2017-10-19 20:23:04 +0300

    income and outcome fragments added and and loaded

--- MoneyIO/app/src/main/java/io/money/moneyio/activities/HomeActivity.java
+++ MoneyIO/app/src/main/java/io/money/moneyio/activities/HomeActivity.java
@@ -1,8 +1,9 @@
 package io.money.moneyio.activities;
 
-import android.content.Intent;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.Color;
-import android.media.Image;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,36 +11,69 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
 import io.money.moneyio.R;
+import io.money.moneyio.fragments.Fragment_Income;
+import io.money.moneyio.fragments.Fragment_Outcome;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private String firstName, secondName;
+    private Button outcome, income;
     private ImageButton sandwichButton;
+    private  DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Fragment_Outcome fragmentOutcome = new Fragment_Outcome();
+        loadFragment(fragmentOutcome);
         getSupportActionBar().hide();
+        outcome = (Button)findViewById(R.id.home_outcome_btn);
+        income = (Button)findViewById(R.id.home_income_btn);
         firebaseAuth = FirebaseAuth.getInstance();
         firstName = getIntent().getStringExtra("firstName");
         secondName = getIntent().getStringExtra("secondName");
         FirebaseUser user = firebaseAuth.getCurrentUser();
         drawerDropMenuCreator();
+        outcomeDrawerMenuBtnListener();
+        incomeDrawerMenuBtnListener();
+    }
+
+    public void incomeDrawerMenuBtnListener() {
+        income.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment_Income fragmentIncome = new Fragment_Income();
+                loadFragment(fragmentIncome);
+                if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+                    drawerLayout.closeDrawer(Gravity.LEFT);
+                }
+            }
+        });
+    }
+
+    public void outcomeDrawerMenuBtnListener() {
+        outcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment_Outcome fragmentOutcome = new Fragment_Outcome();
+                loadFragment(fragmentOutcome);
+                if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+                    drawerLayout.closeDrawer(Gravity.LEFT);
+                }
+            }
+        });
     }
 
     public void drawerDropMenuCreator() {
         sandwichButton = (ImageButton)findViewById(R.id.home_toolbar_sandwich_btn);
-        final DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.dlContent);
+        drawerLayout = (DrawerLayout)findViewById(R.id.dlContent);
 
         //drawer menu settings
         drawerLayout.setScrimColor(Color.TRANSPARENT);
@@ -56,5 +90,12 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.home_main,fragment);
+        ft.commit();
     }
 }
