e106e0d96242cda73b3142725ce2b2e4376cbc1c
Ivan <roni10b@abv.bg>
2017-10-22 23:06:04 +0300

    fragment data history added, icon about data history added and its icon in drawable

---
+++ MoneyIO/app/src/main/java/io/money/moneyio/fragments/Fragment_DataHistory.java
@@ -0,0 +1,22 @@
+package io.money.moneyio.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.money.moneyio.R;
+
+public class Fragment_DataHistory extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_datahistory, container, false);
+        return view;
+    }
+}
