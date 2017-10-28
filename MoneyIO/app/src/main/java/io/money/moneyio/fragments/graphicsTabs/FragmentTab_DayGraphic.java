8c0a35fd9358ad02f6cc1d058151f03dbf4e504d
Ivan <roni10b@abv.bg>
2017-10-29 01:54:51 +0300

    sliding bars in graphics menu ready, grapchics visualised, pie and horizontal charts data is passed, combined chart data to be fixed

--- MoneyIO/app/src/main/java/io/money/moneyio/fragments/graphicsTabs/FragmentTab_DayGraphic.java
+++ MoneyIO/app/src/main/java/io/money/moneyio/fragments/graphicsTabs/FragmentTab_DayGraphic.java
@@ -1,28 +1,52 @@
 package io.money.moneyio.fragments.graphicsTabs;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 import io.money.moneyio.R;
 import io.money.moneyio.model.utilities.MoneyFlow;
+import io.money.moneyio.model.utilities.MonthYearPicker;
 import io.money.moneyio.model.utilities.Utilities;
 
 public class FragmentTab_DayGraphic extends Fragment {
 
     private View view;
-    private Calendar calendar;
     private EditText editDate;
     private ArrayList<MoneyFlow> filteredArr;
+    private PieChart pieChart;
+    private BarChart chart;
+    private HorizontalBarChart horizontalBarChart;
+    private ArrayList<MoneyFlow> moneyFlowData; ///filtered arr, equal to Utilities.data onCreate
+    private Calendar calendar;
+    private MonthYearPicker monthYearPicker;
 
     @Nullable
     @Override
@@ -31,11 +55,16 @@ public class FragmentTab_DayGraphic extends Fragment {
         initialiseElements();
         setFilterClickListener();
         filterDataOnStart();
-
+        incomeExpenseDay();
         return view;
     }
 
     private void initialiseElements() {
+        moneyFlowData = Utilities.data;
+        pieChart = (PieChart) view.findViewById(R.id.statistics_income_expense_year_pie);
+        chart = (BarChart) view.findViewById(R.id.statistics_income_expense_year_combined);
+        horizontalBarChart = (HorizontalBarChart)view.findViewById(R.id.statistics_income_expense_year_horizontal_bar_chart);
+        monthYearPicker = new MonthYearPicker(view.getContext());
         calendar = Calendar.getInstance();
         filteredArr = new ArrayList<>();
         editDate = view.findViewById(R.id.graphics_date_edit);
@@ -55,7 +84,7 @@ public class FragmentTab_DayGraphic extends Fragment {
                 long end = start + 1000*60*60*24;
                 filterData(start, end);
                 editDate.setText("Picked: " + dayOfMonth + " / " + (monthOfYear + 1) + " / " + year);
-                ////do something
+                incomeExpenseDay();
                 calendar = Calendar.getInstance();
             }
         };
@@ -89,5 +118,78 @@ public class FragmentTab_DayGraphic extends Fragment {
                 break;
             }
         }
+    }
+
+    private void incomeExpenseDay() {
+        ///build calendar dialog
+                chart.setVisibility(View.GONE);
+                pieChart.setVisibility(View.GONE);
+                horizontalBarChart.setVisibility(View.VISIBLE);
+
+                TreeMap<String, Float> structuredData = new TreeMap<>(new Comparator<String>() {
+                    @Override
+                    public int compare(String s, String t1) {
+                        return s.compareTo(t1);
+                    }
+                });
+                ArrayList<BarEntry> horizontalBarChartArr = new ArrayList<>();
+                final ArrayList<String> names = new ArrayList<String>();
+
+                for (int i = 0; i < filteredArr.size(); i++) {
+                    if (filteredArr.get(i).getExpense().equalsIgnoreCase("true")) {
+                        Log.e("ivan", filteredArr.get(i).getType());
+                        if (!structuredData.containsKey(filteredArr.get(i).getType())) {
+                            structuredData.put(filteredArr.get(i).getType(),filteredArr.get(i).getSum());
+                            Log.e("ivan", filteredArr.get(i).getType() + "--->" + filteredArr.get(i).getSum());
+                           } else {
+                            structuredData.put(filteredArr.get(i).getType(), (structuredData.get(filteredArr.get(i).getType())+filteredArr.get(i).getSum()));
+                        }
+                    }
+                }
+
+                if (structuredData.isEmpty()) {
+                    horizontalBarChart.setVisibility(View.INVISIBLE);
+                    return;
+                }
+
+                if (names.isEmpty()) {
+                 names.addAll(structuredData.keySet());
+                }
+
+                int i = 0;
+                for (Iterator<Map.Entry<String, Float>> iterator = structuredData.entrySet().iterator(); iterator.hasNext();) {
+                    Map.Entry<String, Float> entry = iterator.next();
+                    Log.e("ivan", "test -->" + entry.getKey() + " - " + entry.getValue());
+                    //i*10 => starting position of the bar
+                    horizontalBarChartArr.add(new BarEntry((i*10), entry.getValue()));
+                    i++;
+                }
+
+
+
+                XAxis xAxis = horizontalBarChart.getXAxis();
+                xAxis.setValueFormatter(new IAxisValueFormatter() {
+                    @Override
+                    public String getFormattedValue(float value, AxisBase axis) {
+                        return names.get((int)value % names.size());
+                    }
+                });
+
+                xAxis.setDrawGridLines(true);
+                xAxis.setDrawAxisLine(true);
+
+                BarDataSet barDataSet = new BarDataSet(horizontalBarChartArr, "Day Expenses");
+                barDataSet.setColors(Color.RED);
+
+                BarData data = new BarData(barDataSet);
+                data.setBarWidth(9f);
+                horizontalBarChart.setDoubleTapToZoomEnabled(false);
+                Description a = new Description();
+                a.setText("");
+                xAxis.setDrawGridLines(false); //remove lines X
+                horizontalBarChart.setDescription(a);
+                horizontalBarChart.invalidate();
+                horizontalBarChart.animateY(1200);
+                horizontalBarChart.setData(data);
     }
 }
--- MoneyIO/app/src/main/java/io/money/moneyio/fragments/graphicsTabs/FragmentTab_MonthGraphic.java
+++ MoneyIO/app/src/main/java/io/money/moneyio/fragments/graphicsTabs/FragmentTab_MonthGraphic.java
@@ -1,17 +1,43 @@
 package io.money.moneyio.fragments.graphicsTabs;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 import io.money.moneyio.R;
 import io.money.moneyio.model.utilities.MoneyFlow;
@@ -21,10 +47,14 @@ import io.money.moneyio.model.utilities.Utilities;
 public class FragmentTab_MonthGraphic extends Fragment {
 
     private View view;
-    private Calendar calendar;
     private EditText editDate;
-    private MonthYearPicker monthYearPicker;
     private ArrayList<MoneyFlow> filteredArr;
+    private PieChart pieChart;
+    private BarChart chart;
+    private HorizontalBarChart horizontalBarChart;
+    private ArrayList<MoneyFlow> moneyFlowData; ///filtered arr, equal to Utilities.data onCreate
+    private Calendar calendar;
+    private MonthYearPicker monthYearPicker;
 
     @Nullable
     @Override
@@ -33,11 +63,16 @@ public class FragmentTab_MonthGraphic extends Fragment {
         initialiseElements();
         filterDataOnStart();
         setFilterClickListener();
-
+        incomeExpenseDay();
         return view;
     }
 
     private void initialiseElements() {
+        moneyFlowData = Utilities.data;
+        pieChart = (PieChart) view.findViewById(R.id.statistics_income_expense_year_pie);
+        chart = (BarChart) view.findViewById(R.id.statistics_income_expense_year_combined);
+        horizontalBarChart = (HorizontalBarChart)view.findViewById(R.id.statistics_income_expense_year_horizontal_bar_chart);
+        monthYearPicker = new MonthYearPicker(view.getContext());
         calendar = Calendar.getInstance();
         monthYearPicker = new MonthYearPicker(view.getContext());
         filteredArr = new ArrayList<>();
@@ -72,7 +107,7 @@ public class FragmentTab_MonthGraphic extends Fragment {
                         editDate.setText("Picked: " + monthYearPicker.getSelectedYear() + " / " + (monthYearPicker.getSelectedMonth()+1));
                         calendar = Calendar.getInstance();
                         monthYearPicker = new MonthYearPicker(view.getContext());
-                        ////do something
+                        incomeExpenseDay();
                     }
                 };
 
@@ -108,5 +143,134 @@ public class FragmentTab_MonthGraphic extends Fragment {
                 break;
             }
         }
+    }
+
+    private void incomeExpenseDay() {
+
+        //=======START OF HORIZONTAL BAR CHART=======
+        chart.setVisibility(View.GONE);
+        pieChart.setVisibility(View.VISIBLE);
+        horizontalBarChart.setVisibility(View.VISIBLE);
+
+        TreeMap<String, Float> structuredData = new TreeMap<>(new Comparator<String>() {
+            @Override
+            public int compare(String s, String t1) {
+                return s.compareTo(t1);
+            }
+        });
+        ArrayList<BarEntry> horizontalBarChartArr = new ArrayList<>();
+        final ArrayList<String> names = new ArrayList<String>();
+
+        for (int i = 0; i < filteredArr.size(); i++) {
+            if (filteredArr.get(i).getExpense().equalsIgnoreCase("true")) {
+                Log.e("ivan", filteredArr.get(i).getType());
+                if (!structuredData.containsKey(filteredArr.get(i).getType())) {
+                    structuredData.put(filteredArr.get(i).getType(),filteredArr.get(i).getSum());
+                    Log.e("ivan", filteredArr.get(i).getType() + "--->" + filteredArr.get(i).getSum());
+                } else {
+                    structuredData.put(filteredArr.get(i).getType(), (structuredData.get(filteredArr.get(i).getType())+filteredArr.get(i).getSum()));
+                }
+            }
+        }
+
+        if (structuredData.isEmpty()) {
+            horizontalBarChart.setVisibility(View.INVISIBLE);
+            return;
+        }
+
+        if (names.isEmpty()) {
+            names.addAll(structuredData.keySet());
+        }
+
+        int i = 0;
+        for (Iterator<Map.Entry<String, Float>> iterator = structuredData.entrySet().iterator(); iterator.hasNext();) {
+            Map.Entry<String, Float> entry = iterator.next();
+            Log.e("ivan", "test -->" + entry.getKey() + " - " + entry.getValue());
+            //i*10 => starting position of the bar
+            horizontalBarChartArr.add(new BarEntry((i*10), entry.getValue()));
+            i++;
+        }
+
+
+
+        XAxis xAxis = horizontalBarChart.getXAxis();
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return names.get((int)value % names.size());
+            }
+        });
+
+        xAxis.setDrawGridLines(true);
+        xAxis.setDrawAxisLine(true);
+
+        BarDataSet barDataSet = new BarDataSet(horizontalBarChartArr, "Day Expenses");
+        barDataSet.setColors(Color.RED);
+
+        BarData data = new BarData(barDataSet);
+        data.setBarWidth(9f);
+        horizontalBarChart.setDoubleTapToZoomEnabled(false);
+        Description a = new Description();
+        a.setText("");
+        xAxis.setDrawGridLines(false); //remove lines X
+        horizontalBarChart.setDescription(a);
+        horizontalBarChart.invalidate();
+        horizontalBarChart.animateY(1200);
+        horizontalBarChart.setData(data);
+        //=====END HORIZONTAL BAR CHART======
+
+
+        //====START OF PIE CHART===========
+                pieChart.setUsePercentValues(true);
+                pieChart.setContentDescription("TEST");
+                pieChart.setHoleColor(Color.YELLOW);
+                pieChart.setHoleRadius(5);
+                pieChart.setDrawHoleEnabled(true);
+                pieChart.setRotationEnabled(true);
+                Legend leg = pieChart.getLegend();
+                leg.setPosition(Legend.LegendPosition.ABOVE_CHART_CENTER);
+
+                HashMap<String, Float> structuredData2 = new HashMap<>();
+                ArrayList<MoneyFlow> utilitiesArray = Utilities.data;
+                ArrayList<PieEntry> pieDataSave = new ArrayList<>();
+
+                for (int z = 0; z < utilitiesArray.size(); z++) {
+                    if (structuredData2.containsKey(utilitiesArray.get(z).getExpense())) {
+                        structuredData2.put(utilitiesArray.get(z).getExpense(), structuredData2.get(utilitiesArray.get(z).getExpense())+utilitiesArray.get(z).getSum());
+                    } else {
+                        structuredData2.put(utilitiesArray.get(z).getExpense(),utilitiesArray.get(i).getSum());
+                    }
+                }
+
+                for (Iterator<Map.Entry<String, Float>> iterator = structuredData2.entrySet().iterator(); iterator.hasNext();) {
+                    Map.Entry<String, Float> entry = iterator.next();
+                    Log.e("ivan", "test -->" + entry.getKey() + " - " + entry.getValue());
+                    pieDataSave.add(new PieEntry(entry.getValue(), entry.getKey().equalsIgnoreCase("true") ?  "Income" : "Expense"));
+                }
+
+                PieDataSet pieDataSet = new PieDataSet(pieDataSave, "Income/Expense year");
+                ArrayList<Integer> colors = new ArrayList<>();
+                colors.add(Color.RED);
+                colors.add(Color.GREEN);
+                pieDataSet.setColors(colors);
+                pieDataSet.setSliceSpace(5);
+
+                pieDataSet.setValueTextSize(15f);
+//        pieDataSet.setSelectionShift(15f);
+                pieDataSet.setValueTextColor(Color.BLACK);
+                PieData pieData = new PieData(pieDataSet);
+                pieDataSet.setValueFormatter(new IValueFormatter() {
+                    @Override
+                    public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+                        return (int) Math.floor(value) + "%";
+                    }
+                });
+                Description description = new Description();
+                description.setText("");
+                pieChart.setDescription(description);
+                pieChart.animateY(1200);
+                pieChart.invalidate();
+                pieChart.setData(pieData);
+
     }
 }
