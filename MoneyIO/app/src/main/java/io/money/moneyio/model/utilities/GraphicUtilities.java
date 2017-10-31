8f77168a2907226c4560315bc86b6c4441991160
Ivan <roni10b@abv.bg>
2017-10-31 14:50:30 +0200

    Charts fixed, chart library replaced, data passed successfully, visualisation perfect

--- MoneyIO/app/src/main/java/io/money/moneyio/model/utilities/GraphicUtilities.java
+++ MoneyIO/app/src/main/java/io/money/moneyio/model/utilities/GraphicUtilities.java
@@ -2,6 +2,7 @@ package io.money.moneyio.model.utilities;
 
 
 import android.graphics.Color;
+import android.util.EventLogTags;
 import android.util.Log;
 import android.view.View;
 
@@ -9,7 +10,6 @@ import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -19,14 +19,14 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.formatter.IValueFormatter;
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.DefaultXAxisValueFormatter;
 import com.github.mikephil.charting.formatter.LargeValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -41,10 +41,6 @@ public abstract class GraphicUtilities {
 
 
     public static void combinedBarChart(BarChart chart, List<MoneyFlow> filteredArr){
-        //=======COMBINED BAR CHART=======
-        float barWidth = 0.3f;
-        float barSpace = 0f;
-        float groupSpace = 0.4f;
 
         //Set the chart setting
         chart.setDescription(null);
@@ -54,23 +50,20 @@ public abstract class GraphicUtilities {
         chart.setDrawBarShadow(false);
         chart.setDrawGridBackground(false);
 
-        //create the data for display the graph
-        int groupCount = 12;
-
         ArrayList<String> xVals = new ArrayList();
 
-        xVals.add("Jan    Feb");
-        xVals.add("");
-        xVals.add("Mar    Apr");
-        xVals.add("");
-        xVals.add("May    Jun");
-        xVals.add("");
-        xVals.add("Jul    Aug");
-        xVals.add("");
-        xVals.add("Sep    Oct");
-        xVals.add("");
-        xVals.add("Nov    Dec");
-        xVals.add("");
+        xVals.add("Jan");
+        xVals.add("Feb");
+        xVals.add("Mar");
+        xVals.add("Apr");
+        xVals.add("May");
+        xVals.add("Jun");
+        xVals.add("Jul");
+        xVals.add("Aug");
+        xVals.add("Sep");
+        xVals.add("Oct");
+        xVals.add("Nov");
+        xVals.add("Dec");
 
         float [] income = {0,0,0,0,0,0,0,0,0,0,0,0};
         float [] expense = {0,0,0,0,0,0,0,0,0,0,0,0};
@@ -85,14 +78,15 @@ public abstract class GraphicUtilities {
             }
         }
 
+        Log.e("ivan", Arrays.toString(income) + " -->> \n" +  Arrays.toString(expense));
+
         ArrayList<BarEntry> yVals1 = new ArrayList(); //expense
         ArrayList<BarEntry> yVals2 = new ArrayList(); //income
 
         for(int i =0; i<income.length; i++){
-            yVals1.add(new BarEntry(i+1, (float) expense[i]));
-            yVals2.add(new BarEntry(i+1, (float) income[i]));
+            yVals1.add(new BarEntry((float) expense[i], i));
+            yVals2.add(new BarEntry((float) income[i], i));
         }
-
 
         //draw the graph
         BarDataSet set1, set2;
@@ -100,45 +94,17 @@ public abstract class GraphicUtilities {
         set1.setColor(Color.RED);
         set2 = new BarDataSet(yVals2, "Income");
         set2.setColor(Color.GREEN);
-        BarData data = new BarData(set1, set2);
+
+        ArrayList<IBarDataSet> sets = new ArrayList<>();
+        sets.add(set1);
+        sets.add(set2);
+
+        BarData data = new BarData(xVals, sets);
         data.setValueFormatter(new LargeValueFormatter());
         chart.setData(data);
-        chart.getBarData().setBarWidth(barWidth);
-        chart.getXAxis().setAxisMinimum(0);
-        chart.getXAxis().setAxisMaximum(0 + chart.getBarData().getGroupWidth(groupSpace, barSpace) * groupCount);
-        chart.groupBars(0, groupSpace, barSpace);
-        chart.getData().setHighlightEnabled(false);
-        chart.animateY(1200);
+        chart.getXAxis().setValueFormatter(new DefaultXAxisValueFormatter());
+        chart.animateY(1000);
         chart.invalidate();
-
-        //Draw the indicator
-        Legend l = chart.getLegend();
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
-        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-        l.setDrawInside(true);
-        l.setYOffset(20f);
-        l.setXOffset(0f);
-        l.setYEntrySpace(0f);
-        l.setTextSize(8f);
-
-        //Draw the X-Axis and Y-Axis
-        //X-axis
-        XAxis xAxis = chart.getXAxis();
-        xAxis.setGranularity(1f);
-        xAxis.setGranularityEnabled(true);
-        xAxis.setCenterAxisLabels(true);
-        xAxis.setDrawGridLines(false);
-        xAxis.setAxisMaximum(12);
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(xVals));
-        //Y-axis
-        chart.getAxisRight().setEnabled(false);
-        YAxis leftAxis = chart.getAxisLeft();
-        leftAxis.setValueFormatter(new LargeValueFormatter());
-        leftAxis.setDrawGridLines(true);
-        leftAxis.setSpaceTop(35f);
-        leftAxis.setAxisMinimum(0f);
     }
 
     public static void pieChart(PieChart pieChart, List<MoneyFlow> utilitiesArray){
@@ -152,7 +118,8 @@ public abstract class GraphicUtilities {
         leg.setPosition(Legend.LegendPosition.ABOVE_CHART_CENTER);
 
         HashMap<String, Float> structuredData2 = new HashMap<>();
-        ArrayList<PieEntry> pieDataSave = new ArrayList<>();
+        ArrayList<Entry> pieDataSave = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
 
         for (int z = 0; z < utilitiesArray.size(); z++) {
             if (structuredData2.containsKey(utilitiesArray.get(z).getExpense())) {
@@ -162,12 +129,19 @@ public abstract class GraphicUtilities {
             }
         }
 
+        int i = 0;
         for (Iterator<Map.Entry<String, Float>> iterator = structuredData2.entrySet().iterator(); iterator.hasNext();) {
             Map.Entry<String, Float> entry = iterator.next();
-            pieDataSave.add(new PieEntry(entry.getValue(), entry.getKey().equalsIgnoreCase("true") ?  "Income" : "Expense"));
+            pieDataSave.add(new Entry(entry.getValue(), i));
+            if (entry.getKey().equalsIgnoreCase("true")) {
+                names.add("Income");
+            } else {
+                names.add("Expense");
+            }
+            i++;
         }
 
-        PieDataSet pieDataSet = new PieDataSet(pieDataSave, "Income/Expense year");
+        PieDataSet pieDataSet = new PieDataSet(pieDataSave, "- Income/Expense");
         ArrayList<Integer> colors = new ArrayList<>();
         colors.add(Color.RED);
         colors.add(Color.GREEN);
@@ -175,39 +149,31 @@ public abstract class GraphicUtilities {
         pieDataSet.setSliceSpace(5);
 
         pieDataSet.setValueTextSize(15f);
-//        pieDataSet.setSelectionShift(15f);
         pieDataSet.setValueTextColor(Color.BLACK);
-        PieData pieData = new PieData(pieDataSet);
-        pieDataSet.setValueFormatter(new IValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
-                return (int) Math.floor(value) + "%";
-            }
-        });
-        Description description = new Description();
-        description.setText("");
-        pieChart.setDescription(description);
-        pieChart.animateY(1200);
-        pieChart.invalidate();
+        PieData pieData = new PieData(names, pieDataSet);
+        pieData.setValueFormatter(new PercentFormatter());
+        pieChart.setDescription("");
         pieChart.setData(pieData);
+        pieChart.animateY(1000);
+        pieChart.invalidate();
     }
 
     public static void horizontalBarChart(HorizontalBarChart horizontalBarChart, List<MoneyFlow> filteredArr){
+
         TreeMap<String, Float> structuredData = new TreeMap<>(new Comparator<String>() {
             @Override
             public int compare(String s, String t1) {
                 return s.compareTo(t1);
             }
         });
+
         ArrayList<BarEntry> horizontalBarChartArr = new ArrayList<>();
-        final ArrayList<String> names = new ArrayList<String>();
+        final List<String> names = new ArrayList<String>();
 
         for (int i = 0; i < filteredArr.size(); i++) {
             if (filteredArr.get(i).getExpense().equalsIgnoreCase("true")) {
-                Log.e("ivan", filteredArr.get(i).getType());
                 if (!structuredData.containsKey(filteredArr.get(i).getType())) {
                     structuredData.put(filteredArr.get(i).getType(),filteredArr.get(i).getSum());
-                    Log.e("ivan", filteredArr.get(i).getType() + "--->" + filteredArr.get(i).getSum());
                 } else {
                     structuredData.put(filteredArr.get(i).getType(), (structuredData.get(filteredArr.get(i).getType())+filteredArr.get(i).getSum()));
                 }
@@ -219,44 +185,28 @@ public abstract class GraphicUtilities {
             return;
         }
 
-        if (names.isEmpty()) {
-            names.addAll(structuredData.keySet());
-        }
-
         int i = 0;
+        ArrayList<Float> values = new ArrayList<>();
         for (Iterator<Map.Entry<String, Float>> iterator = structuredData.entrySet().iterator(); iterator.hasNext();) {
             Map.Entry<String, Float> entry = iterator.next();
-            Log.e("ivan", "test -->" + entry.getKey() + " - " + entry.getValue());
-            //i*10 => starting position of the bar
-            horizontalBarChartArr.add(new BarEntry((i*10), entry.getValue()));
-            i++;
+            names.add(entry.getKey());
+            values.add(entry.getValue());
         }
 
+        int valuesLen = values.size();
+        horizontalBarChart.getLayoutParams().height = valuesLen * 50;
 
+        for (int z = 0; z < values.size(); z++) {
+            horizontalBarChartArr.add(new BarEntry(values.get(z), z));
+        }
 
-        XAxis xAxis = horizontalBarChart.getXAxis();
-        xAxis.setValueFormatter(new IAxisValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return names.get((int)value % names.size());
-            }
-        });
-
-        xAxis.setDrawGridLines(true);
-        xAxis.setDrawAxisLine(true);
-
-        BarDataSet barDataSet = new BarDataSet(horizontalBarChartArr, "Day Expenses");
-        barDataSet.setColors(Color.RED);
-
-        BarData data = new BarData(barDataSet);
-        data.setBarWidth(9f);
+        BarDataSet bardataset = new BarDataSet(horizontalBarChartArr, "Expenses");
+        bardataset.setColor(Color.RED);
+        BarData data = new BarData(names, bardataset);
         horizontalBarChart.setDoubleTapToZoomEnabled(false);
-        Description a = new Description();
-        a.setText("");
-        xAxis.setDrawGridLines(false); //remove lines X
-        horizontalBarChart.setDescription(a);
+        horizontalBarChart.setDescription("");
         horizontalBarChart.invalidate();
-        horizontalBarChart.animateY(1200);
+        horizontalBarChart.animateY(1000);
         horizontalBarChart.setData(data);
     }
 }
