1c70af2b5998ba1538b863ae40c4f0ea93803e9c
mironov <aleksandar.n.mironov@gmail.com>
2017-11-13 21:23:32 +0200

    pediod tab logic added

--- MoneyIO/app/src/main/java/io/money/moneyio/fragments/graphicsTabs/FragmentTab_PeriodGraphic.java
+++ MoneyIO/app/src/main/java/io/money/moneyio/fragments/graphicsTabs/FragmentTab_PeriodGraphic.java
@@ -1,34 +1,87 @@
 package io.money.moneyio.fragments.graphicsTabs;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.PieChart;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import io.money.moneyio.R;
+import io.money.moneyio.model.MoneyFlow;
+import io.money.moneyio.model.database.DatabaseHelperFirebase;
+import io.money.moneyio.model.utilities.GraphicUtilities;
+import io.money.moneyio.model.utilities.Utilities;
 
 public class FragmentTab_PeriodGraphic extends Fragment {
 
     private View view;
-    private EditText calendarTo;
+    private EditText calendarTo, calendarFrom;
     private TextView from, to;
+    private DatabaseHelperFirebase fdb;
+    private List<MoneyFlow> filteredArr;
+    private PieChart pieChart;
+    private BarChart chart;
+    private HorizontalBarChart horizontalBarChart;
+    private Calendar calendar;
+    private Spinner spinner;
+    private long start, end;
+    private int spinnerPosition;
+    private TextView income, expense, overall;
+    private ImageView questionPie, questionCombined, questionHorizontal, plusMinus;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.tab_fragment_graphics, container, false);
         initialise();
+        setStartEnd();
+        setFilterClickListener();
+        setSpinnerSettings();
+        setQuestionListeners();
         return view;
     }
 
     private void initialise() {
+        calendar = Calendar.getInstance();
         calendarTo = view.findViewById(R.id.graphics_date_edit1);
+        calendarTo.setText(calendar.get(Calendar.YEAR) + " / " +
+                (calendar.get(Calendar.MONTH)+1) + " / " +  calendar.get(Calendar.DAY_OF_MONTH));
         from = view.findViewById(R.id.graphics_text_from);
         to = view.findViewById(R.id.graphics_text_to);
+        fdb = DatabaseHelperFirebase.getInstance(view.getContext());
+        pieChart = view.findViewById(R.id.statistics_income_expense_year_pie);
+        chart = view.findViewById(R.id.statistics_income_expense_year_combined);
+        horizontalBarChart = view.findViewById(R.id.statistics_income_expense_year_horizontal_bar_chart);
+        filteredArr = new ArrayList<>();
+        calendarFrom = view.findViewById(R.id.graphics_date_edit);
+        calendarFrom.setText(calendar.get(Calendar.YEAR) + " / " +
+                (calendar.get(Calendar.MONTH)+1) + " / " +  calendar.get(Calendar.DAY_OF_MONTH));
+        spinner = view.findViewById(R.id.statistics_spinner_menu);
+        spinnerPosition = 0;
+        income = view.findViewById(R.id.statistics_income_bar);
+        expense = view.findViewById(R.id.statistics_expense_bar);
+        overall = view.findViewById(R.id.statistics_overall_bar);
+        questionPie = view.findViewById(R.id.statistics_question_pie);
+        questionCombined = view.findViewById(R.id.statistics_question_combined);
+        questionHorizontal = view.findViewById(R.id.statistics_question_horizontal_bar);
+        plusMinus = view.findViewById(R.id.ststistics_plusminus);
         setVisibility();
     }
 
@@ -36,5 +89,108 @@ public class FragmentTab_PeriodGraphic extends Fragment {
         calendarTo.setVisibility(View.VISIBLE);
         to.setVisibility(View.VISIBLE);
         from.setVisibility(View.VISIBLE);
+        chart.setVisibility(View.GONE);
+        questionCombined.setVisibility(View.GONE);
+        pieChart.setVisibility(View.GONE);
+        questionPie.setVisibility(View.GONE);
+        horizontalBarChart.setVisibility(View.VISIBLE);
+        questionHorizontal.setVisibility(View.VISIBLE);
+    }
+
+    private void setStartEnd(){
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0,0,0);
+        start = calendar.getTimeInMillis();
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 23, 59, 59);
+        end = calendar.getTimeInMillis();
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0,0,0);
+    }
+
+    public void setQuestionListeners(){
+        questionHorizontal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utilities.displayPopupWindow(v, getString(R.string.horizontal_bar_chart) + getString(R.string.hbc_day_text));
+            }
+        });
+    }
+
+    private void setSpinnerSettings() {
+        //creating spinner adapter
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.history_spinner, R.layout.support_simple_spinner_dropdown_item);
+        //set the theme of the spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked);
+        //setting the spinner's adapter
+        spinner.setAdapter(adapter);
+
+        //spinner item selected listener functionality
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spinnerPosition = position;
+                filteredArr = fdb.filterData(start, end, spinnerPosition);
+                incomeExpensePeriod();
+            }
+
+            public void onNothingSelected(AdapterView<?> parent){
+            }
+        });
+    }
+
+    private void setFilterClickListener(){
+        //date picker settings
+        final DatePickerDialog.OnDateSetListener fromDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                calendar.set(year, monthOfYear, dayOfMonth, 0,0,0);
+                start = calendar.getTimeInMillis();
+                filteredArr = fdb.filterData(start, end, spinnerPosition);
+                calendarFrom.setText(dayOfMonth + " / " + (monthOfYear + 1) + " / " + year);
+                calendar = Calendar.getInstance();
+                incomeExpensePeriod();
+            }
+        };
+
+        calendarFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+
+                calendar.setTimeInMillis(start);
+                new DatePickerDialog(view.getContext(), fromDate, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        final DatePickerDialog.OnDateSetListener toDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                calendar.set(year, monthOfYear, dayOfMonth, 23, 59, 59);
+                end = calendar.getTimeInMillis();
+                filteredArr = fdb.filterData(start, end, spinnerPosition);
+                calendarTo.setText(dayOfMonth + " / " + (monthOfYear + 1) + " / " + year);
+                calendar = Calendar.getInstance();
+                incomeExpensePeriod();
+            }
+        };
+
+        calendarTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+
+                calendar.setTimeInMillis(end);
+                new DatePickerDialog(view.getContext(), toDate, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    //start the charts and set them visible/gone
+    private void incomeExpensePeriod() {
+        GraphicUtilities.dataFilerForCurrentTab(income, expense, overall, filteredArr, plusMinus);
+        GraphicUtilities.horizontalBarChart(horizontalBarChart, filteredArr, questionHorizontal);
     }
 }
--- MoneyIO/app/src/main/java/io/money/moneyio/fragments/statisticsTabs/FragmentTab_Period.java
+++ MoneyIO/app/src/main/java/io/money/moneyio/fragments/statisticsTabs/FragmentTab_Period.java
@@ -1,40 +1,159 @@
 package io.money.moneyio.fragments.statisticsTabs;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import io.money.moneyio.R;
+import io.money.moneyio.model.MoneyFlow;
+import io.money.moneyio.model.database.DatabaseHelperFirebase;
+import io.money.moneyio.model.recyclers.HistoryRecyclerViewAdapter;
 
 public class FragmentTab_Period extends Fragment {
 
     private View view;
-    private EditText calendarTo;
+    private EditText calendarFrom, calendarTo;
     private TextView from, to;
+    private DatabaseHelperFirebase fdb;
+    private RecyclerView recyclerView;
+    private Calendar calendar;
+    private List<MoneyFlow> filteredArr;
+    private Spinner spinner;
+    private long start, end;
+    private int spinnerPosition;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.tab_fragment_datahistory, container, false);
         initialise();
+        setStartEnd();
+        setFilter();
+        setSpinnerSettings();
         return view;
     }
 
     private void initialise() {
+        calendar = Calendar.getInstance();
+        fdb = DatabaseHelperFirebase.getInstance(view.getContext());
+        recyclerView = view.findViewById(R.id.history_recycler_view);
+        filteredArr = new ArrayList<>();
+        calendarFrom = view.findViewById(R.id.history_date_edit);
+        calendarFrom.setText(calendar.get(Calendar.YEAR) + " / " +
+                (calendar.get(Calendar.MONTH)+1) + " / " +  calendar.get(Calendar.DAY_OF_MONTH));
+        spinner = view.findViewById(R.id.history_spinner);
+        spinnerPosition = 0;
         calendarTo = view.findViewById(R.id.history_date_edit1);
+        calendarTo.setText(calendar.get(Calendar.YEAR) + " / " +
+                (calendar.get(Calendar.MONTH)+1) + " / " +  calendar.get(Calendar.DAY_OF_MONTH));
         from = view.findViewById(R.id.history_text_from);
         to = view.findViewById(R.id.history_text_to);
         setVisibility();
+    }
+
+    private void setStartEnd(){
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0,0,0);
+        start = calendar.getTimeInMillis();
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 23, 59, 59);
+        end = calendar.getTimeInMillis();
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0,0,0);
     }
 
     private void setVisibility() {
         calendarTo.setVisibility(View.VISIBLE);
         to.setVisibility(View.VISIBLE);
         from.setVisibility(View.VISIBLE);
+    }
+    private void setSpinnerSettings() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.history_spinner, R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spinnerPosition = position;
+                filteredArr = DatabaseHelperFirebase.filterData(start, end, spinnerPosition);
+                startRecycler(filteredArr);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent){
+
+            }
+        });
+    }
+
+    private void setFilter(){
+
+        final DatePickerDialog.OnDateSetListener fromDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                calendar.set(year, monthOfYear, dayOfMonth, 0,0,0);
+                start = calendar.getTimeInMillis();
+                filteredArr = fdb.filterData(start, end, spinnerPosition);
+                startRecycler(filteredArr);
+                calendarFrom.setText(dayOfMonth + " / " + (monthOfYear + 1) + " / " + year);
+                calendar = Calendar.getInstance();
+            }
+        };
+
+        calendarFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+
+                calendar.setTimeInMillis(start);
+                new DatePickerDialog(view.getContext(), fromDate, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        final DatePickerDialog.OnDateSetListener toDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                calendar.set(year, monthOfYear, dayOfMonth, 23, 59, 59);
+                end = calendar.getTimeInMillis();
+                filteredArr = fdb.filterData(start, end, spinnerPosition);
+                startRecycler(filteredArr);
+                calendarTo.setText(dayOfMonth + " / " + (monthOfYear + 1) + " / " + year);
+                calendar = Calendar.getInstance();
+            }
+        };
+
+        calendarTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+
+                calendar.setTimeInMillis(end);
+                new DatePickerDialog(view.getContext(), toDate, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void startRecycler(List<MoneyFlow> data) {
+        HistoryRecyclerViewAdapter adapter = new HistoryRecyclerViewAdapter(view.getContext(), data);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(adapter);
     }
 }
